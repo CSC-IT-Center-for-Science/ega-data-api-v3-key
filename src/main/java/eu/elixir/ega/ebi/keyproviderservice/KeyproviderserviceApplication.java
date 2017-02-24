465533ec83fad677047fffcbd18f44ba05717e07
asenf <asenf@asenf-virtual-machine>
2017-02-24 10:17:35 +0000

    Added Swagger

--- src/main/java/eu/elixir/ega/ebi/keyproviderservice/KeyproviderserviceApplication.java
+++ src/main/java/eu/elixir/ega/ebi/keyproviderservice/KeyproviderserviceApplication.java
@@ -20,15 +20,32 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @SpringBootApplication
 @EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+@EnableSwagger2
 @EnableDiscoveryClient
 public class KeyproviderserviceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KeyproviderserviceApplication.class, args);
 	}
+        
+        @Bean
+        public Docket swaggerSettings() {
+            return new Docket(DocumentationType.SWAGGER_2)
+                    .select()
+                    .apis(RequestHandlerSelectors.any())
+                    .paths(PathSelectors.any())
+                    .build()
+                    .pathMapping("/");
+        }
 }
